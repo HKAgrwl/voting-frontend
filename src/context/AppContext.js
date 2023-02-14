3c7a4ab485b791b30997aa31810a35865c3a5e40
HKAgrwl <harshkumaragarwal2003@gmail.com>
2023-02-14 16:01:14 +0530

    14Feb

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,30 +1,30 @@
-import React from 'react'
-import { useState,useContext } from 'react'
+// import React from 'react'
+// import { useState,useContext } from 'react'
 
-const initialState={
-    name:'',
-    email:'',
-    password:'',
-}
+// const initialState={
+//     name:'',
+//     email:'',
+//     password:'',
+// }
 
-const AppContext = React.createContext()
+// const AppContext = React.createContext()
 
-const AppProvider = ({children})=>{
+// const AppProvider = ({children})=>{
 
     const addUserToLocalStorage = ({user}) => {
         localStorage.setItem('user',JSON.stringify(user))
     }
 
-    const registerUser= async(currentUser)=>{
-        try{
-            addUserToLocalStorage({currentUser})
-        }catch(err){
-            console.log(err);
-            alert("SOmething Went Wrong");
-        }
-    }
+//     const registerUser= async(currentUser)=>{
+//         try{
+//             addUserToLocalStorage({currentUser})
+//         }catch(err){
+//             console.log(err);
+//             alert("SOmething Went Wrong");
+//         }
+//     }
 
-    return <AppContext.Provider>
-        {children}
-    </AppContext.Provider>
-}
+//     return <AppContext.Provider>
+//         {children}
+//     </AppContext.Provider>
+// }
